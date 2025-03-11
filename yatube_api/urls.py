5388657204ce961a725b39289aa37479e29d7360
sergeiBogdano <164154142+sergeiBogdano@users.noreply.github.com>
2025-03-11 12:25:38 +0400

    Add files via upload

---
+++ yatube_api/urls.py
@@ -0,0 +1,14 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from api.views import CommentViewSet, GroupViewSet, PostViewSet
+
+router = DefaultRouter()
+router.register(r'posts', PostViewSet, basename='posts')
+router.register(r'comments', CommentViewSet, basename='comments')
+router.register(r'groups', GroupViewSet, basename='groups')
+
+urlpatterns = [
+    path('api/v1/', include('api.urls')),
+    path('api/', include('api.urls')),
+]
