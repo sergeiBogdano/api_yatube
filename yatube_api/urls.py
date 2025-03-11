68cea7d78668f096089381bb7a2e754cecad507a
sergeiBogdano <164154142+sergeiBogdano@users.noreply.github.com>
2025-03-11 12:39:45 +0400

    Update urls.py

--- yatube_api/urls.py
+++ yatube_api/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from api.views import CommentViewSet, GroupViewSet, PostViewSet
