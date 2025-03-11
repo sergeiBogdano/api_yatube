9d43cbee31641231fbdb7d1b35c55866055e92e2
sergeiBogdano <164154142+sergeiBogdano@users.noreply.github.com>
2025-03-11 12:36:17 +0400

    Update views.py

--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404
 
-from rest_framework import permissions, status, viewsets
+from rest_framework import permissions, viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
