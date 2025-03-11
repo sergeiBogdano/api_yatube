4a385b221baa04b62fcec140ad8d69b586c6ff76
sergeiBogdano <164154142+sergeiBogdano@users.noreply.github.com>
2025-03-11 12:29:51 +0400

    Update settings.py

--- yatube_api/settings.py
+++ yatube_api/settings.py
@@ -56,14 +56,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'yatube_api.wsgi.application'
 
 
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 
 AUTH_PASSWORD_VALIDATORS = [
@@ -91,7 +89,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 
 STATIC_URL = '/static/'
