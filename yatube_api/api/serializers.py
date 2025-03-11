0ced4bbb7803d8fe7b2f259fec8660002acbef0c
sergeiBogdano <164154142+sergeiBogdano@users.noreply.github.com>
2025-03-12 02:34:34 +0400

    Update serializers.py

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -10,16 +10,13 @@ class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Post
-        fields = (
-            'id', 'text', 'author', 'image', 'group', 'pub_date'
-        )
-        read_only_fields = ('author', 'pub_date')
+        fields = '__all__'
 
 
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
-        fields = ('id', 'title', 'slug', 'description')
+        fields = '__all__'
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -29,5 +26,5 @@ class CommentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Comment
-        fields = ('id', 'author', 'post', 'text', 'created')
-        read_only_fields = ('author', 'post', 'created')
+        fields = '__all__'
+        read_only_fields = ('post',)
