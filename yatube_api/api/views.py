84f273952aabf0294fe2ec3aa30ba079b945e384
sergeiBogdano <164154142+sergeiBogdano@users.noreply.github.com>
2025-03-12 11:26:34 +0400

    Update views.py

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,37 +1,31 @@
 from django.shortcuts import get_object_or_404
+
 from rest_framework import permissions, viewsets
 from rest_framework.authentication import TokenAuthentication
 
-from .serializers import (CommentSerializer, GroupSerializer, PostSerializer)
-from posts.models import Group, Post
 from .permissions import IsOwnerOrReadOnly
+from .serializers import CommentSerializer, GroupSerializer, PostSerializer
+from posts.models import Group, Post
+
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.IsAuthenticated, IsOwnerOrReadOnly)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
-    def destroy(self, request, *args, **kwargs):
-        instance = self.get_object()
-        self.check_object_permissions(request, instance)
-        return super().destroy(request, *args, **kwargs)
-
 
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
-    authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.IsAuthenticated,)
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.IsAuthenticated, IsOwnerOrReadOnly)
 
     def get_post(self):
@@ -42,8 +36,3 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user, post=self.get_post())
-
-    def destroy(self, request, *args, **kwargs):
-        instance = self.get_object()
-        self.check_object_permissions(request, instance)
-        return super().destroy(request, *args, **kwargs)
