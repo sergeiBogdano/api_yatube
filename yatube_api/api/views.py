f293c55711d4d3e7e0eef6929f4f9ba39e27a94c
sergeiBogdano <164154142+sergeiBogdano@users.noreply.github.com>
2025-03-12 11:27:41 +0400

    Update views.py

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import get_object_or_404
 
 from rest_framework import permissions, viewsets
-from rest_framework.authentication import TokenAuthentication
 
 from .permissions import IsOwnerOrReadOnly
 from .serializers import CommentSerializer, GroupSerializer, PostSerializer
 from posts.models import Group, Post
-
 
 
 class PostViewSet(viewsets.ModelViewSet):
